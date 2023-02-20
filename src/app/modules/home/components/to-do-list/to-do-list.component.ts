39e8ddea44df825d953bdff20edad04b186c3019
Felipe Araujo <felipe.araujo@capgemini.com>
2023-02-20 20:38:57 -0300

    input working with some validations

--- src/app/modules/home/components/to-do-list/to-do-list.component.ts
+++ src/app/modules/home/components/to-do-list/to-do-list.component.ts
@@ -9,13 +9,15 @@ import { TaskList } from '../../model/task-list';
 export class ToDoListComponent implements OnInit {
 
   public taskList: Array<TaskList> = [
-    { task: "My task 1", checked: true },
-    { task: "My task 2", checked: false },
   ];
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  public setEmitTaskList(event: string) {
+    this.taskList.push({task: event, checked: false});
   }
 
   public deleteItemTaskList(event: number) {
