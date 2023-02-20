d7bd9fe2a42852bd944d6efa0d5d58147c2340bb
Felipe Araujo <felipe.araujo@capgemini.com>
2023-02-20 20:14:06 -0300

    buttons working with initial logic

--- src/app/modules/home/components/to-do-list/to-do-list.component.ts
+++ src/app/modules/home/components/to-do-list/to-do-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { TaskList } from '../../model/task-list';
 
 @Component({
   selector: 'app-to-do-list',
@@ -7,9 +8,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ToDoListComponent implements OnInit {
 
+  public taskList: Array<TaskList> = [
+    { task: "My task 1", checked: true },
+    { task: "My task 2", checked: false },
+  ];
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  public deleteItemTaskList(event: number) {
+    this.taskList.splice(event, 1);
+  }
+
+  public deleteAllTaskList() {
+    const confirmation = confirm("Do you really want to erase all tasks?");
+
+    if(confirmation) {
+      this.taskList = [];
+    }
   }
 
 }
